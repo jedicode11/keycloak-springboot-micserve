7f1cc82ec4917fd504a74fcf58b6c19ca66a0e56
Yasen Apostolov <y.apostolov@trackfast.net>
2022-10-20 11:29:54 +0300

    Stable Connected Version

---
+++ src/main/java/com/example/demo/KeycloakSpringbootMserveApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages={"com.example.demo.controller"} )
+public class KeycloakSpringbootMserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakSpringbootMserveApplication.class, args);
+	}
+}
